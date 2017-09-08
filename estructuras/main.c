6f0208d93aebdce2be67d7de37a87150e6fd4e5b
jony <jonyduck@hotmail.es>
2017-09-08 12:21:44 -0300

    Add files via upload

---
+++ estructuras/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int codigo;
+    char descripcion[50]
+    float precio;
+    char laboratorio[30];
+}eMedicamento;
+
+int main()
+{
+
+eMedicamento listamedicamentos[3];
+int codigo [3];
+float precio[3];
+char descripcion
+
+
+
+
+
+}return 0;
